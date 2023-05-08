15a7bea61a2fe6f071eb6415d733e8472056b15a
PuvaanRaaj <112681813+PuvaanRaaj@users.noreply.github.com>
2023-05-08 16:40:56 +0800

    fixed error

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const baseURL = "http://localhost:3000/api/v1";
+// const baseURL = "http://localhost:3000/api/v1";
+const baseURL = "https://sofware-project-management-project.onrender.com/api/v1";
 
 export default baseURL;
