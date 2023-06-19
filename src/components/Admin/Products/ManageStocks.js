9e329d62417e21153619491eef021efd8a1cc910
Puvaan Raaj <112681813+PuvaanRaaj@users.noreply.github.com>
2023-06-19 14:29:09 +0000

    managestock

--- src/components/Admin/Products/ManageStocks.js
+++ src/components/Admin/Products/ManageStocks.js
@@ -10,7 +10,7 @@ import ErrorMsg from "../../ErrorMsg/ErrorMsg";
 import LoadingComponent from "../../LoadingComp/LoadingComponent";
 import NoDataFound from "../../NoDataFound/NoDataFound";
 
-export default function ManageStocks() {
+const ManageStocks = () => {
   const dispatch = useDispatch();
 
   // Delete product handler
@@ -30,14 +30,14 @@ export default function ManageStocks() {
     products: { products },
     loading,
     error,
-  } = useSelector((state) => state?.products);
+  } = useSelector((state) => state.products);
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">
-            Product List- [{products?.length}]{" "}
+            Product List- [{products?.length}]
           </h1>
           <p className="mt-2 text-sm text-gray-700">
             List of all the products in your account including their name,
@@ -178,13 +178,13 @@ export default function ManageStocks() {
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
-                              stroke-width="1.5"
+                              strokeWidth={1.5}
                               stroke="currentColor"
                               className="w-6 h-6"
                             >
                               <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                               />
                             </svg>
@@ -224,4 +224,6 @@ export default function ManageStocks() {
       )}
     </div>
   );
-}
+};
+
+export default ManageStocks;
