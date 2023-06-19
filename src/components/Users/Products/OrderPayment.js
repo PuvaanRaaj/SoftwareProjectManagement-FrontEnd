63a2d7e2c5afe07a9ca6e2ad268c449ec251429f
Puvaan Raaj <112681813+PuvaanRaaj@users.noreply.github.com>
2023-06-19 07:15:44 +0000

    order payment page

--- src/components/Users/Products/OrderPayment.js
+++ src/components/Users/Products/OrderPayment.js
@@ -95,7 +95,7 @@ export default function OrderPayment() {
 
                           <div className="flex flex-1 items-end justify-between pt-2">
                             <p className="mt-1 text-sm font-medium text-gray-900">
-                              $ {product?.price} X {product?.qty} =$
+                              RM {product?.price} X {product?.qty} =$
                               {product?.totalPrice}
                             </p>
                           </div>
@@ -113,7 +113,7 @@ export default function OrderPayment() {
                     <div className="flex items-center justify-between border-t border-gray-200 pt-6">
                       <dt className="text-base font-medium">Sub Total</dt>
                       <dd className="text-base font-medium text-gray-900">
-                        $ {sumTotalPrice}.00
+                        RM {sumTotalPrice}
                       </dd>
                     </div>
                   </dl>
@@ -125,7 +125,7 @@ export default function OrderPayment() {
                       <button
                         onClick={placeOrderHandler}
                         className="w-full rounded-md border border-transparent bg-indigo-600 py-3 px-4 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
-                        Confirm Payment - ${sumTotalPrice}
+                        Confirm Payment - RM {sumTotalPrice}
                       </button>
                     )}
                   </div>
